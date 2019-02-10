e50055f5548c4cdad0a10cdc2d76de7e6ad6ba64
Amanda <aeverett1317@gmail.com>
2019-02-10 16:53:07 -0700

    created AdventuringContainer and Adventuring component.

--- client/src/components/Adventure.js
+++ client/src/components/Adventure.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import AdventuringContainer from '../redux/containers/AdventuringContainer'
 import {
     Card, CardImg, CardText, CardBody, CardLink,
     CardTitle, CardSubtitle,
@@ -49,18 +50,7 @@ class Adventure extends Component {
                                 <CardLink> <Link to={`/adventures/${adventure.adventure_id}`} >Go On Adventure!</Link> </CardLink>
                                 <Button color="danger" onClick={this.toggle}>Start</Button>
 
-                                {/* Modal */}
-                                <Modal isOpen={this.state.modal} modalTransition={{ timeout: 700 }} backdropTransition={{ timeout: 1300 }}
-                                    toggle={this.toggle} className={this.props.className}>
-                                    <ModalHeader toggle={this.toggle}>Modal title</ModalHeader>
-                                    <ModalBody>
-                                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                                    </ModalBody>
-                                    <ModalFooter>
-                                        <Button color="primary" onClick={this.toggle}>Do Something</Button>{' '}
-                                        <Button color="secondary" onClick={this.toggle}>Cancel</Button>
-                                    </ModalFooter>
-                                </Modal>
+                                <AdventuringContainer />
                             </CardBody>
                         </Card>
                     </Col>
